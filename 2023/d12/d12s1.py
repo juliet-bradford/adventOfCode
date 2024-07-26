d125c7f0a3a489e54b8b215671c789139b5c9bbd
Juliet Bradford <julietallisonbradford@gmail.com>
2024-07-26 16:46:03 -0400

    this is a really horrible solution. like its so bad that I ran it and sat there for 5 seconds and thought "oh its got an infinite loop somewhere" but then it returned. I really out did myself

--- 2023/d12/d12s1.py
+++ 2023/d12/d12s1.py
@@ -1,5 +1,21 @@
+def isValid (dRec, cRec):
+    return [len(i) for i in dRec.split('.') if i != ''] == cRec
+    
 
-input_file = "test.txt"
+
+def numberOfArrangements (dRec: str, cRec):
+    arraSum = 0
+
+    if dRec.count('?') == 0:
+        return 1 if isValid(dRec, cRec) else 0
+    
+    arraSum += numberOfArrangements(dRec.replace('?', '#', 1), cRec)
+    arraSum += numberOfArrangements(dRec.replace('?', '.', 1), cRec)
+
+    return arraSum
+
+
+input_file = "input.txt"
 
 with open(input_file, "r") as data:
     damagedRecords = data.readlines()
@@ -7,7 +23,10 @@ with open(input_file, "r") as data:
 contiguousRecords = [[int(num) for num in record.split(' ')[1].strip('\n').split(',')] for record in damagedRecords]
 damagedRecords = [record.split(' ')[0] for record in damagedRecords]
 
-for i in damagedRecords:
-    print(i)
-for i in contiguousRecords:
-    print(i)
+# try some backtracking
+sumOfArrangements = 0
+for dRec, cRec in zip(damagedRecords, contiguousRecords):
+    sumOfArrangements += numberOfArrangements(dRec, cRec)
+
+print("What is the sum of those counts?")
+print(sumOfArrangements)
