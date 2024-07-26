69b79e8143b77fd8131d337c5c62c1c70030dac1
Juliet Bradford <julietallisonbradford@gmail.com>
2024-07-26 17:50:37 -0400

    gonna be honest. kinda feeling good right now. locked in and made an actual backtracking solution. I think this can work for part 2

--- 2023/d12/d12s1.py
+++ 2023/d12/d12s1.py
@@ -1,16 +1,48 @@
-def isValid (dRec, cRec):
-    return [len(i) for i in dRec.split('.') if i != ''] == cRec
+
+from re import sub
+
+def isValid (springs, groupLengths):
+    return [len(i) for i in springs.split('.') if i != ''] == groupLengths
     
 
 
-def numberOfArrangements (dRec: str, cRec):
+def numberOfArrangements (springs: str, groupLengths, currentGroupLength):
+    
+    if len(springs) == 0 and (len(groupLengths) != 1 or currentGroupLength != groupLengths[0]):
+        return 0 
+
     arraSum = 0
 
-    if dRec.count('?') == 0:
-        return 1 if isValid(dRec, cRec) else 0
-    
-    arraSum += numberOfArrangements(dRec.replace('?', '#', 1), cRec)
-    arraSum += numberOfArrangements(dRec.replace('?', '.', 1), cRec)
+    # if we have no group yet
+    if currentGroupLength == 0:
+        if springs[0] == '.':
+            arraSum += numberOfArrangements(springs[1:], groupLengths, currentGroupLength)
+        elif springs[0] == '#':
+            arraSum += numberOfArrangements(springs[1:], groupLengths, 1)
+        else:
+            arraSum += numberOfArrangements(springs[1:], groupLengths, currentGroupLength)
+            arraSum += numberOfArrangements(springs[1:], groupLengths, 1)
+
+
+    # if our group is not done yet
+    elif currentGroupLength < groupLengths[0]:
+        if springs[0] == '.':
+            return 0
+        else:
+            arraSum += numberOfArrangements(springs[1:], groupLengths, currentGroupLength+1)
+
+    # if we have completed our group
+    else:
+        # if we are at the end, validity test and return
+        if len(groupLengths) == 1:
+            return 1 if springs.count('#') == 0 else 0
+        
+        # if next is '#' then we fucked up
+        if springs[0] == '#':
+            return 0
+        # if next is '?' we have to treat it as '.', and start new group
+        else:
+            arraSum += numberOfArrangements(springs[1:], groupLengths[1:], 0)
 
     return arraSum
 
@@ -20,13 +52,13 @@ input_file = "input.txt"
 with open(input_file, "r") as data:
     damagedRecords = data.readlines()
 
-contiguousRecords = [[int(num) for num in record.split(' ')[1].strip('\n').split(',')] for record in damagedRecords]
-damagedRecords = [record.split(' ')[0] for record in damagedRecords]
+groupLengthsList = [[int(num) for num in record.split(' ')[1].strip('\n').split(',')] for record in damagedRecords]
+springsList = [sub('\.\.+','.', record.split(' ')[0].strip('.')) for record in damagedRecords]
 
 # try some backtracking
 sumOfArrangements = 0
-for dRec, cRec in zip(damagedRecords, contiguousRecords):
-    sumOfArrangements += numberOfArrangements(dRec, cRec)
+for springs, groupLengths in zip(springsList, groupLengthsList):
+    sumOfArrangements += numberOfArrangements(springs, groupLengths, 0)
 
 print("What is the sum of those counts?")
 print(sumOfArrangements)
